94d722d70d7d37e24b3340a42b59c2ffed499257
yinquan <616244978@qq.com>
2019-10-02 18:53:32 +0800

    一些节奏型的绘制

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div id="app">
     <div class="metronome-container">
     </div>
-    <div class="row" :key=i v-for="(note, i) in notes">
-      <MusicalNote :description="note.description"></MusicalNote>
+    <div class="rp-row" :key=i v-for="(row, i) in rhymePatternLayout">
+      <div class="rp-col" :style="{width: 100 / row.length + '%'}" :key=j v-for="(rp, j) in row">
+        <MusicalNote :description="description" :key=k v-for="(description, k) in notes[rp]['descriptions']"></MusicalNote>
+      </div>
     </div>
   </div>
 </template>
@@ -24,26 +26,123 @@ export default {
     return {
       notes: {
         '1/4': {
-          description: 'stem,head hollow',
+          descriptions: ['stem,head'],
           rhymePattern: '1'
         },
-        '1/4_1/4': {
-          description: 'stem,head|hline one|stem,head,hline one',
+        '1/8_1/8': {
+          descriptions: ['stem,head|hline one|hline  one|stem,head,hline one'],
           rhymePattern: '12'
         },
         '1/16_1/16_1/16_1/16': {
-          description: 'stem,head|w1o3@stem,head,hline one,hline two|w1o3@stem,head,hline one,hline two|w1o3@stem,head,hline one,hline two',
+          descriptions: ['stem,head|w1o3@stem,head,hline one,hline two|w1o3@stem,head,hline one,hline two|w1o3@stem,head,hline one,hline two'],
+          rhymePattern: '1222'
+        },
+        '1/16_1/16_1/16_1/16_1/4': {
+          descriptions: ['stem,head|w1o3@stem,head,hline one,hline two|w1o3@stem,head,hline one,hline two|w1o3@stem,head,hline one,hline two', 'stem,head'],
           rhymePattern: '1222'
         },
         '1/12_1/12_1/12': {
-          description: 'stem,head|hline one|stem,head,hline one|hline one|stem,head,hline one',
+          descriptions: ['tuple t3>stem,head|hline one|stem,head,hline one|hline one|stem,head,hline one'],
           rhymePattern: '122'
         },
+        '1/12_1/12_1/12_1/4': {
+          descriptions: ['tuple t3>stem,head|hline one|stem,head,hline one|hline one|stem,head,hline one', 'stem,head'],
+          rhymePattern: '122,1'
+        },
+        '1/20_1/20_1/20_1/20_1/20': {
+          descriptions: ['tuple t5>stem,head|w1o5@hline one,stem,head|w1o5@hline one,stem,head|w1o5@hline one,stem,head|w1o5@hline one,stem,head'],
+          rhymePaterrn: '12222'
+        },
+        '1/20_1/20_1/20_1/20_1/20_1/4': {
+          descriptions: ['tuple t5>stem,head|w1o5@hline one,stem,head|w1o5@hline one,stem,head|w1o5@hline one,stem,head|w1o5@hline one,stem,head', 'stem,head'],
+          rhymePaterrn: '12222,1'
+        },
         '3/16_1/16': {
-          description: 'stem,head|hline one,dot|hline one|stem,head,hline one,hline two',
+          descriptions: ['stem,head|hline one,dot|hline one|stem,head,hline one,hline two'],
           rhymePattern: '1002'
+        },
+        '3/16_1/16_1/4': {
+          descriptions: ['stem,head|hline one,dot|hline one|stem,head,hline one,hline two', 'stem,head'],
+          rhymePattern: '1002,1'
+        },
+        '1/16_3/16': {
+          descriptions: ['stem,head|hline one,hline two|w1o5@hline one,stem,head|dot'],
+          rhymePattern: '1200'
+        },
+        '1/16_3/16_1/4': {
+          descriptions: ['stem,head|hline one,hline two|w1o5@hline one,stem,head|dot', 'stem,head'],
+          rhymePattern: '1200,1'
+        },
+        '1/8_1/16_1/16': {
+          descriptions: ['stem,head|w1o5@stem,head,hline one|w1o5@stem,head,hline one,hline two'],
+          rhymePattern: '1022'
+        },
+        '1/8_1/16_1/16_1/4': {
+          descriptions: ['stem,head|w1o5@stem,head,hline one|w1o5@stem,head,hline one,hline two', 'stem,head'],
+          rhymePattern: '1022,1'
+        },
+        '1/16_1/16_1/8': {
+          descriptions: ['stem,head|w1o5@stem,head,hline one, hline two|w1o5@stem,head,hline one'],
+          rhymePattern: '1220'
+        },
+        '1/16_1/16_1/8_1/4': {
+          descriptions: ['stem,head|w1o5@stem,head,hline one, hline two|w1o5@stem,head,hline one', 'stem,head'],
+          rhymePattern: '1220,1'
+        },
+        '1/16_1/8_1/16': {
+          descriptions: ['stem,head|hline one,hline two|w1o5@hline one,stem,head|w1o5@hline one|hline one,hline two,stem,head'],
+          rhymePattern: '1202'
+        },
+        '1/16_1/8_1/16_1/4': {
+          descriptions: ['stem,head|hline one,hline two|w1o5@hline one,stem,head|w1o5@hline one|hline one,hline two,stem,head', 'stem,head'],
+          rhymePattern: '1202,1'
+        },
+        '1/8_1/4_1/8': {
+          descriptions: ['stem,head|flag', 'stem,head', 'stem,head|flag'],
+          rhymePattern: '12,02'
+        },
+        '1/8_1/4_1/8_1/4': {
+          descriptions: ['stem,head|flag', 'stem,head', 'stem,head|flag', 'stem,head'],
+          rhymePattern: '12,02,1'
+        },
+        '3/32_1/32_1/16_1/16': {
+          descriptions: ['stem,head|hline one,hline two,dot|w1o2@stem,head,hline one,hline two,hline three|w1o5@stem,head,hline one, hline two|w1o5@stem,head,hline one, hline two'],
+          rhymePattern: '10022020'
+        },
+        '3/32_1/32_1/16_1/16_1/4': {
+          descriptions: ['stem,head|hline one,hline two,dot|w1o2@stem,head,hline one,hline two,hline three|w1o5@stem,head,hline one, hline two|w1o5@stem,head,hline one, hline two', 'stem,head'],
+          rhymePattern: '10022020,1'
+        },
+        '3/8_1/16_1/16': {
+          descriptions: ['stem,head|dot', 'stem,head|w1o5@hline one,hline two,stem,head'],
+          rhymePattern: '1,0022'
+        },
+        '3/8_1/16_1/16_1/4': {
+          descriptions: ['stem,head|dot', 'stem,head|w1o5@hline one,hline two,stem,head', 'stem,head'],
+          rhymePattern: '1,0022,1'
+        },
+        '1/16_1/16_3/8': {
+          descriptions: ['stem,head|w1o5@hline one,hline two,stem,head', 'stem,head|dot'],
+          rhymePattern: '1220,0'
+        },
+        '1/16_1/16_3/8_1/4': {
+          descriptions: ['stem,head|w1o5@hline one,hline two,stem,head', 'stem,head|dot', 'stem,head'],
+          rhymePattern: '1220,0,1'
+        },
+        '1/8': {
+          descriptions: ['stem,head|flag'],
+          rhymePattern: '1'
         }
-      }
+      },
+      rhymePatternLayout: [
+        ['1/4', '1/8_1/8'],
+        ['1/16_1/16_1/16_1/16', '1/16_1/16_1/16_1/16_1/4', '3/32_1/32_1/16_1/16', '3/32_1/32_1/16_1/16_1/4'],
+        ['1/12_1/12_1/12', '1/12_1/12_1/12_1/4', '1/20_1/20_1/20_1/20_1/20', '1/20_1/20_1/20_1/20_1/20_1/4'],
+        ['3/16_1/16', '3/16_1/16_1/4', '1/16_3/16', '1/16_3/16_1/4'],
+        ['1/16_1/16_1/8', '1/16_1/16_1/8_1/4', '1/8_1/16_1/16', '1/8_1/16_1/16_1/4'],
+        ['1/16_1/8_1/16', '1/16_1/8_1/16_1/4', '1/8_1/4_1/8', '1/8_1/4_1/8_1/4'],
+        ['3/8_1/16_1/16', '3/8_1/16_1/16_1/4', '1/16_1/16_3/8', '1/16_1/16_3/8_1/4']
+      ]
     }
   }
 }
@@ -64,8 +163,29 @@ export default {
   overflow: hidden;
   border: 1px solid red;
 }
-.row {
+.rp-row {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-around;
+  align-items: flex-start;
+  align-content: flex-start;
+  width: 100%;
+  height: 50px;
+  border: 1px 0 0 0;
+  border-top: 1px solid red;
+}
+.rp-col {
+  flex-grow: 1;
+  flex-shrink: 1;
   display: block;
-  margin-bottom: 20px;
+  border: 0 1px 0 0;
+  border-right: 1px solid red;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  height: calc(100% - 30pxd w);
+}
+.rp-col:last-child {
+  border-right: 0;
 }
 </style>
--- src/components/MusicalNote.vue
+++ src/components/MusicalNote.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="['note', draw.noteClass]">
+  <div :class="['note', draw.noteClass]" :style="cssProps">
     <div :class="['piece', piece.class]" v-for="(piece, i) in draw.pieces" :key=i>
       <div :class="[part]" v-for="(part, j) in piece.parts" :key=j></div>
     </div>
@@ -13,9 +13,23 @@ export default {
     description: {
       type: String,
       default: ''
+    },
+    color: {
+      type: String,
+      default: 'black'
+    },
+    backgroundColor: {
+      type: String,
+      default: 'white'
     }
   },
   computed: {
+    cssProps() {
+      return {
+        '--color': this.color,
+        '--backgroundColor': this.backgroundColor
+      }
+    },
     draw() {
       console.log('description: ' + this.description + '\n');
       let desc = this.description;
@@ -54,15 +68,17 @@ export default {
 
 <style scoped>
 .note {
-  display: flex;
+  display: inline-flex;
   flex-direction: row;
   flex-wrap: nowrap;
   justify-content: flex-start;
   align-items: flex-start;
   align-content: flex-start;
+  vertical-align: top;
   width: auto;
   height: auto;
   padding: 0;
+  margin-right: 10px;
 }
 .piece {
   width: 10px;
@@ -71,7 +87,11 @@ export default {
   border: 0;
   margin: 0;
   padding: 0;
+  z-index: 3;
   position: relative;
+}
+.piece.w0o8 {
+  width: 8px;
 }
 .piece.w0o5 {
   width: 5px;
@@ -81,6 +101,9 @@ export default {
 }
 .piece.w1o5 {
   width: 15px;
+}
+.piece.w2 {
+  width: 20px;
 }
 .stem {
   display: block;
@@ -92,11 +115,11 @@ export default {
   position: absolute;
   right: 0;
   top: 0;
-  background-color: black;
+  background-color: var(--color);
 }
 .head {
   width: 10px;
-  background: black;
+  background: var(--color);
   border-radius: 200%;
   height: 6px;
   position: absolute;
@@ -107,19 +130,55 @@ export default {
 .head.hollow::before {
   content: '';
   display: block;
-  width: 6px;
-  background: white;
+  width: 7px;
+  background: var(--backgroundColor);
   border-radius: 200%;
   position: absolute;
   height: 2px;
   bottom: 2px;
   right: 2px;
 }
+.flag {
+  width: 10px;
+  height: 18px;
+  overflow: hidden;
+  border: 0;
+  display: block;
+  position: relative;
+}
+.flag::before {
+  content: '';
+  display: block;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  transform: scale(1, 1);
+  position: absolute;
+  left: -13px;
+  top: 6px;
+  border: 0;
+  background-color: var(--backgroundColor);
+  z-index: 5;
+}
+.flag::after {
+  content: '';
+  display: block;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  transform: scale(1, 1.1);
+  position: absolute;
+  left: -12px;
+  top: 4px;
+  border: 0;
+  background-color: var(--color);
+  z-index: 3;
+}
 .hline {
   width: 100%;
   height: 3px;
   border: 0;
-  background-color: black;
+  background-color: var(--color);
   position: absolute;
   left: 0;
 }
@@ -128,5 +187,54 @@ export default {
 }
 .hline.two {
   top: 6px;
+}
+.hline.three {
+  top: 12px;
+}
+.dot {
+  height: 3px;
+  width: 3px;
+  background-color: var(--color);
+  border-radius: 50%;
+  display: block;
+  position: absolute;
+  left: 50%;
+  bottom: -3px;
+}
+.tuple {
+  position: relative;
+}
+.tuple::before {
+  content: '';
+  display: block;
+  width: calc(100% - 8px);
+  height: 100px;
+  border-radius: 8px;
+  border-top: 1px solid var(--color);
+  position: absolute;
+  left: 8px;
+  top: -7px;
+  z-index: 1;
+}
+.tuple::after {
+  content: '';
+  position: absolute;
+  font-size: 12px;
+  left: calc(4px + 50% - 0.3em);
+  top: calc(-7px - 0.5em);
+  font-weight: bold;
+  z-index: 2;
+  background-color: var(--backgroundColor);
+}
+.tuple.t3::after {
+  content: '3';
+}
+.tuple.t5::after {
+  content: '5';
+}
+.tuple.t6::after {
+  content: '6';
+}
+.empty {
 }
 </style>
